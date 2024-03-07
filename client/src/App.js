e39591ae94c8afcca44200459f8cf3b90ffd2ac6
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-03-07 00:51:12 -0600

    Updated code in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import PostsList from './PostsList';
-import CreatePost from './CreatePost'; // Import CreatePost component
-import logo from './logo.svg';
+import PostsList from './PostsList'; // Component to list your posts
+import CreatePost from './CreatePost'; // Component to create a new post
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Personal Blog Platform</h1>
       </header>
-      <CreatePost /> {/* Include the CreatePost component */}
-      <PostsList /> {/* PostsList component to list posts */}
+      <main>
+        <CreatePost /> {/* Allow users to create a new post */}
+        <PostsList /> {/* Show a list of posts */}
+      </main>
     </div>
   );
 }
