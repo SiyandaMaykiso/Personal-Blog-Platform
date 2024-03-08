a3c6a5e241a920923251372a5dff3f45054ae891
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-03-08 01:01:53 -0600

    Update db.js file

--- server/db.js
+++ server/db.js
@@ -1,12 +1,7 @@
-const { Pool } = require('pg');
-require('dotenv').config(); 
-
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME, // This line is updated to match the .env file
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
-
-module.exports = pool;
