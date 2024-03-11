96f5bc689d1dae56e5dba82ff555cccd291f2f9f
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-03-11 13:53:31 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PostsList from './PostsList'; // Component to list your posts
 import CreatePost from './CreatePost'; // Component to create a new post
 import Login from './Login'; // Import the Login component
+import Registration from './Registration'; // Import the Registration component
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       </header>
       <main>
         <Login /> {/* Add the Login component to your application */}
+        <Registration /> {/* Add the Registration component to your application */}
         <CreatePost /> {/* Allow users to create a new post */}
         <PostsList /> {/* Show a list of posts */}
       </main>
