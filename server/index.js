e8e32e172dcea4a5272df8d8f6d7653e22c1396b
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-03-19 16:43:24 -0500

    Add postinstall script to build React app

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const session = require('express-session'); 
 const cors = require('cors');
-const path = require('path'); // Add this line
+const path = require('path');
 const isAuthenticated = require('./middleware/isAuthenticated');
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -32,11 +32,11 @@ const authRoutes = require('./routes/authRoutes');
 app.use('/posts', isAuthenticated, postsRoutes);
 app.use('/auth', authRoutes);
 
-// Serve static files from the React app
+
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
+
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'../client/build/index.html'));
 });
