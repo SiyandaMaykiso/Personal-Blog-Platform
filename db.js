dc8a292b058de279eb8113e2252f5d007e9bad5d
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-04-29 15:47:13 -0500

    Update db.js for heroku production

--- db.js
+++ db.js
@@ -1,12 +1,28 @@
 const { Pool } = require('pg');
-require('dotenv').config(); 
+require('dotenv').config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+let poolConfig;
+
+if (process.env.NODE_ENV === 'production') {
+    // In production on Heroku, use DATABASE_URL
+    poolConfig = {
+        connectionString: process.env.DATABASE_URL,
+        // Ensuring SSL connection is required for secure connection to Heroku Postgres
+        ssl: {
+            rejectUnauthorized: false // Necessary to avoid "self-signed certificate" errors
+        }
+    };
+} else {
+    // In development, use details from .env file
+    poolConfig = {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT
+    };
+}
+
+const pool = new Pool(poolConfig);
 
 module.exports = pool;
