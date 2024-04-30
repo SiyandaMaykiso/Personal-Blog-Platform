993dd4011e0150036607ace2816ea8fdd4d689dd
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-04-30 14:50:53 -0500

    Update authentication

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,11 +1,20 @@
+const jwt = require('jsonwebtoken');
+
 function isAuthenticated(req, res, next) {
-  console.log("Checking authentication, session:", req.session);
-  if (req.session.user) {
-      next();
-  } else {
-      console.log("Unauthorized access attempt, no user found in session.");
-      res.status(401).json({ message: 'You are not authorized to view this page' });
-  }
+    const token = req.headers.authorization?.split(' ')[1]; // Assumes Bearer token is used
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) {
+                console.log("Token verification failed:", err);
+                return res.status(401).json({ message: 'Invalid token' });
+            }
+            req.user = decoded; // Add the decoded user to the request object
+            next();
+        });
+    } else {
+        console.log("Unauthorized access attempt, no token provided.");
+        res.status(401).json({ message: 'No token provided, authorization denied' });
+    }
 }
-  module.exports = isAuthenticated;
-  
+
+module.exports = isAuthenticated;
