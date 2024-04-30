7b0915fb4ab979b1431a5b55ae57e7f1f9ed38c5
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-04-29 22:18:09 -0500

    Refactor home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -5,10 +5,14 @@ import Registration from './Registration';
 function Home({ onLogin, onRegistration }) {
   return (
     <div className="home-container">
-      <h1>Welcome to the Personal Blog Platform</h1>
-      <div className="auth-container">
-        <Login onLogin={onLogin} />
-        <Registration onRegistration={onRegistration} />
+      <h1>Welcome to Personal Blog Platform</h1>
+      <div className="authentication-container">
+        <div className="login">
+          <Login onLogin={onLogin} />
+        </div>
+        <div className="registration">
+          <Registration onRegistration={onRegistration} />
+        </div>
       </div>
     </div>
   );
