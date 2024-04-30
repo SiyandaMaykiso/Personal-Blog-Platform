dc301ef36a1672320a696b475118af7573dee1d4
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-04-29 22:50:17 -0500

    Update navigation

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import Login from './Login';
 import Registration from './Registration';
+import { useNavigate } from 'react-router-dom';
 
 function Home({ onLogin, onRegistration }) {
+  const navigate = useNavigate();
+
+  const handleLogin = (userData) => {
+    onLogin(userData); // Perform login
+    navigate('/create-post'); // Navigate to create post page
+  };
+
+  const handleRegistration = (userData) => {
+    onRegistration(userData); // Perform registration
+    navigate('/create-post'); // Navigate to create post page
+  };
+
   return (
     <div className="home-container">
       <h1>Welcome to Personal Blog Platform</h1>
-      <div className="authentication-container">
-        <div className="login">
-          <Login onLogin={onLogin} />
-        </div>
-        <div className="registration">
-          <Registration onRegistration={onRegistration} />
-        </div>
+      <div className="auth-container">
+        <Login onLogin={handleLogin} />
+        <Registration onRegistration={handleRegistration} />
       </div>
     </div>
   );
