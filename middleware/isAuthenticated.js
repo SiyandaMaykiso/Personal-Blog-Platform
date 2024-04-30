806db57c5246daa564191a52e8ed477e37c4c4d7
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-04-30 13:39:23 -0500

    Console log

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,11 +1,11 @@
 function isAuthenticated(req, res, next) {
-    if (req.session.user) {
+  console.log("Checking authentication, session:", req.session);
+  if (req.session.user) {
       next();
-    } else {
-    
+  } else {
+      console.log("Unauthorized access attempt, no user found in session.");
       res.status(401).json({ message: 'You are not authorized to view this page' });
-    }
   }
-  
+}
   module.exports = isAuthenticated;
   
