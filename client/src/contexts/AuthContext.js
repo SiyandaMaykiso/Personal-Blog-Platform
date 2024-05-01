a8e4111ebaf020cf868d53715c0af8b6d637fac4
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-05-01 04:18:37 -0500

    authcontext

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -31,14 +31,14 @@ export const AuthProvider = ({ children }) => {
   const login = async (username, password) => {
     setLoading(true);
     try {
-      const response = await fetch('https://personal-blog-platform-a11db04dd963.herokuapp.com/login', {
+      const response = await fetch('https://personal-blog-platform-a11db04dd963.herokuapp.com/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password }), // Changed from email to username
+        body: JSON.stringify({ username, password }),
       });
-  
+
       if (!response.ok) throw new Error('Failed to log in');
-  
+
       const data = await response.json();
       setCurrentUser(data.user);
       setAuthToken(data.token);
@@ -48,12 +48,12 @@ export const AuthProvider = ({ children }) => {
     } finally {
       setLoading(false);
     }
-  };  
+  };
 
   const register = async (username, email, password) => {
     setLoading(true);
     try {
-      const response = await fetch('https://personal-blog-platform-a11db04dd963.herokuapp.com/register', {
+      const response = await fetch('https://personal-blog-platform-a11db04dd963.herokuapp.com/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, email, password }),
