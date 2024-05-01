43dfbcca80a545aabe81fee4e0bee557c1f8ed31
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-05-01 01:07:56 -0500

    tokenservice

--- client/src/services/tokenService.js
+++ client/src/services/tokenService.js
@@ -4,7 +4,7 @@
 export const setToken = (token) => {
     localStorage.setItem('jwtToken', token);
     // Set the token to axios default header directly after setting it in local storage
-    import('./services/axiosConfig').then(axiosConfig => {
+    import('./axiosConfig').then(axiosConfig => {
       axiosConfig.default.defaults.headers.common['Authorization'] = `Bearer ${token}`;
     });
   };
@@ -17,7 +17,7 @@ export const setToken = (token) => {
   // Removes the token from local storage and axios headers
   export const removeToken = () => {
     localStorage.removeItem('jwtToken');
-    import('./services/axiosConfig').then(axiosConfig => {
+    import('./axiosConfig').then(axiosConfig => {
       delete axiosConfig.default.defaults.headers.common['Authorization'];
     });
   };
