1d9aeb837f723082d7087d3a6bdfdd3cb7f8de35
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-05-01 00:26:50 -0500

    Update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from './services/axiosConfig'; // Use your configured Axios instance that handles headers
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import PostsList from './PostsList';
 import CreatePost from './CreatePost';
@@ -12,26 +12,31 @@ function App() {
   const [token, setToken] = useState(localStorage.getItem('jwtToken'));
 
   useEffect(() => {
-    // Ensure token is not null before making the request
     if (token) {
-      axios.get('https://personal-blog-platform-a11db04dd963.herokuapp.com/auth/session')
-        .then(response => {
-          setUser(response.data.user);
-          console.log('Session validated', response.data);
-        })
-        .catch(error => {
-          console.log('Session check failed:', error);
-          setUser(null);
-          setToken(null);
-          localStorage.removeItem('jwtToken'); // Remove invalid or expired token
-        });
+      // Delay the session check slightly to ensure local storage has updated
+      setTimeout(() => {
+        axios.get('https://personal-blog-platform-a11db04dd963.herokuapp.com/auth/session')
+          .then(response => {
+            setUser(response.data.user);
+            console.log('Session validated', response.data);
+          })
+          .catch(error => {
+            console.log('Session check failed:', error);
+            setUser(null);
+            setToken(null);
+            localStorage.removeItem('jwtToken'); // Remove invalid or expired token
+          });
+      }, 100); // Delay of 100 milliseconds
     }
   }, [token]); // Dependency array includes token to trigger effect when it changes
 
   const handleUserLogin = (authData) => {
     localStorage.setItem('jwtToken', authData.token);
-    setToken(authData.token); // Set token in state to trigger useEffect
-    setUser(authData.user);
+    console.log('Token set in local storage:', localStorage.getItem('jwtToken')); // Confirm token is set
+    setTimeout(() => {
+      setToken(authData.token); // Set token in state to trigger useEffect after a brief delay
+      setUser(authData.user);
+    }, 100); // Wait 100ms to update state to ensure localStorage is synced
   };
 
   const handleLogout = () => {
